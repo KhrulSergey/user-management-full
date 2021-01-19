4f0e9e2edf52830e11d54b16be05027914910647
Sergey Khrul <siberianodis@gmail.com>
2021-01-19 11:48:52 +0200

    1. Minor fixes

--- src/main/java/com/khsa/usermanagement/util/UserComparator.java
+++ src/main/java/com/khsa/usermanagement/util/UserComparator.java
@@ -10,6 +10,7 @@ import static java.util.Objects.nonNull;
 @Component
 public class UserComparator implements Comparator<User> {
 
+    //TODO fix method and implement using
     @Override
     public int compare(User o1, User o2) {
         if (nonNull(o1.getUsername()) && nonNull(o2.getUsername())) {
